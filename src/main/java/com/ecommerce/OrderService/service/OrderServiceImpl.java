326b69fa682f9631e6fd4e75b8e45f71cd0418b7
Shivakumar <shivakumargani8@gmail.com>
2024-09-06 21:08:43 +0530

    Enahancing error handling

--- src/main/java/com/ecommerce/OrderService/service/OrderServiceImpl.java
+++ src/main/java/com/ecommerce/OrderService/service/OrderServiceImpl.java
@@ -14,7 +14,6 @@ import com.ecommerce.OrderService.model.OrderStatus;
 import com.ecommerce.OrderService.repository.IOrderRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
